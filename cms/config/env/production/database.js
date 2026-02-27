7860a2253fbde98c6057272f1104b1436a7b2f2d
Prarthan Agarwal <75473832+prarthanagarwal@users.noreply.github.com>
2026-02-27 21:51:53 +0530

    feat(cms): configure postgres database for production deployment

---
+++ cms/config/env/production/database.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+    connection: {
+        client: "postgres",
+        connection: {
+            host: env("DATABASE_HOST"),
+            port: env.int("DATABASE_PORT", 5432),
+            database: env("DATABASE_NAME", "strapi"),
+            user: env("DATABASE_USERNAME", "strapi"),
+            password: env("DATABASE_PASSWORD", ""),
+            ssl: env.bool("DATABASE_SSL", false),
+        },
+        pool: {
+            min: env.int("DATABASE_POOL_MIN", 2),
+            max: env.int("DATABASE_POOL_MAX", 10),
+        },
+    },
+});

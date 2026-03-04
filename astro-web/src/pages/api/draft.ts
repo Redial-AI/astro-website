0a6580a0525482744fdbf4be4067384505b4b7dc
Prarthan Agarwal <75473832+prarthanagarwal@users.noreply.github.com>
2026-03-05 01:50:50 +0530

    fix: final hopefully

--- astro-web/src/pages/api/draft.ts
+++ astro-web/src/pages/api/draft.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from "astro";
 
+export const prerender = false;
+
 export const GET: APIRoute = ({ url, cookies, redirect }) => {
     const documentId = url.searchParams.get("documentId");
     const path = url.searchParams.get("path") || "/";
